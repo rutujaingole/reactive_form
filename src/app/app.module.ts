8c2142c149b8d943f93c548fc94fb7b6785a3da5
rutujaingole <72335505+rutujaingole@users.noreply.github.com>
2022-06-28 20:42:48 +0530

    modified files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import {ReactiveFormsModule} from '@angular/forms'
+import { HelloComponent } from './hello.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,ReactiveFormsModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent] 
+  imports:      [ BrowserModule, FormsModule ],
+  declarations: [ AppComponent, HelloComponent ],
+  bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
